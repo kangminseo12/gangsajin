630862031c31f235247733a8442ad190d9c3f210
MinJoongWon <wnddnjs4593@gmail.com>
2023-10-01 10:22:47 +0900

    [fix] model, 수정하기

--- carrotproject/carrotapp/views.py
+++ carrotproject/carrotapp/views.py
@@ -19,19 +19,23 @@ import uuid
 
 # Create your views here.
 
+
 def main(request):
-    list_post = PostProduct.objects.filter(status="N").order_by('-view')
-    return render(request, "dangun_app/main.html", {'posts': list_post})
+    list_post = PostProduct.objects.filter(status="N").order_by("-view")
+    return render(request, "dangun_app/main.html", {"posts": list_post})
 
 
 def trade(request):
-    top_views_posts = PostProduct.objects.filter(status="N").order_by('-view')
-    return render(request, 'dangun_app/trade.html', {'posts': top_views_posts})
+    top_views_posts = PostProduct.objects.filter(status="N").order_by("-view")
+    return render(request, "dangun_app/trade.html", {"posts": top_views_posts})
+
 
 def search(request):
     search_data = request.GET.get("search")
-    search_list = PostProduct.objects.filter(Q(title__icontains=search_data)|Q(location__icontains=search_data)) 
-    return render(request, "dangun_app/search.html", {'posts': search_list})
+    search_list = PostProduct.objects.filter(
+        Q(title__icontains=search_data) | Q(location__icontains=search_data)
+    )
+    return render(request, "dangun_app/search.html", {"posts": search_list})
 
 
 @login_required
@@ -62,7 +66,7 @@ def edit(request, id):
         if "thumbnail" in request.FILES:
             post.thumbnail = request.FILES["thumbnail"]
         post.save()
-        return redirect("dangun_app:trade_post", pk=id)
+        return redirect("dangun_app:trade_post", post_id=id)
 
     return render(request, "dangun_app/write.html", {"post": post})
 
@@ -160,6 +164,7 @@ def chatroom_list(request):
     # chat 화면을 처음 들어왔을 경우 채팅방 리스트를 뿌려줌
     user = request.user
 
+
 def get_chatrooms_context(user):
     # 현재 로그인한 사용자가 chat_host 또는 chat_guest인 ChatRoom을 검색
     chatrooms = ChatRoom.objects.filter(Q(chat_host=user.id) | Q(chat_guest=user.id))
@@ -182,27 +187,25 @@ def get_chatrooms_context(user):
         last_message = Message.objects.filter(chatroom_id=chatroom.id).order_by("-sent_at").first()
 
         result = {
-          
-            'chatroom' : chatroom, # 채팅방 정보
-            'chat_partner' : chat_partner, # 채팅 상대방의 정보
-            'product' : product, # 상품 정보
-            'message' : last_message # 마지막 메시지 정보
-
+            "chatroom": chatroom,  # 채팅방 정보
+            "chat_partner": chat_partner,  # 채팅 상대방의 정보
+            "product": product,  # 상품 정보
+            "message": last_message,  # 마지막 메시지 정보
         }
 
         chatrooms_context.append(result)
-    
+
     return chatrooms_context
 
 
 @login_required
 def chatroom_list(request):
     user = request.user
-    
+
     # 참여하고 있는 채팅방 목록 및 관련 정보 불러오기
     chatrooms_context = get_chatrooms_context(user)
-    
-    return render(request, 'dangun_app/chat.html', {'chatrooms' : chatrooms_context})
+
+    return render(request, "dangun_app/chat.html", {"chatrooms": chatrooms_context})
 
 
 @login_required
@@ -211,7 +214,7 @@ def chatroom(request, chatroom_id):
 
     # 참여하고 있는 채팅방 목록 및 관련 정보 불러오기
     chatrooms_context = get_chatrooms_context(user)
-    
+
     # 클릭한 채팅방 및 채팅 상대방에 대한 정보
     selected_chatroom = ChatRoom.objects.get(id=chatroom_id)
     if selected_chatroom.chat_host == user.id:
@@ -230,20 +233,19 @@ def chatroom(request, chatroom_id):
     product = PostProduct.objects.get(id=selected_chatroom.product_id)
 
     # 주고받은 채팅(메시지) 기록
-    messages = Message.objects.filter(chatroom=chatroom_id).order_by('sent_at')
+    messages = Message.objects.filter(chatroom=chatroom_id).order_by("sent_at")
 
     # WebSocket 연결을 위한 주소
     ws_path = f"/ws/chat/{selected_chatroom.id}"
 
     # 템플릿에 전달할 데이터 정의
     context = {
-        'chatrooms' : chatrooms_context,
-        "selected_chatroom" : selected_chatroom,
-        "product" : product,
-        "chat_partner" : chat_partner,
-        "messages" : messages,
-        "ws_path" : ws_path,
+        "chatrooms": chatrooms_context,
+        "selected_chatroom": selected_chatroom,
+        "product": product,
+        "chat_partner": chat_partner,
+        "messages": messages,
+        "ws_path": ws_path,
     }
 
     return render(request, "dangun_app/chat.html", context)
-
