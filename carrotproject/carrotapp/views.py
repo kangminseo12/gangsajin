f1ebc098d070a5bcea860a6a082e9a555fbccc37
jhlee <makk7201@gmail.com>
2023-10-05 13:26:05 +0900

    채팅보기 버튼 기능, 메시지 줄바꿈, 기타
    [채팅보기]
    - 한 상품 채팅방이 여러 개인 경우 가장 최근에 생성된 채팅방으로 입장
    - 상품에 대해 생성된 채팅방이 없을 경우 채팅 목록 화면으로 이동
    - 로그인하지 않은 유저인 경우 로그인 화면으로 이동
    
    [메시지 줄바꿈]
    - shift + enter 누르면 줄바꿈 되도록 수정
    - 웹소켓으로 수신한 메시지 및 db에서 불러온 메시지에 줄바꿈 적용
    
    [기타]
    - 스크롤바 디자인 수정
    - 온도에 도씨 추가
    - 물품 가격 표기 형식 변경
    - 상품 이미지 사이즈 조절
    - 채팅목록에서 최근 메시지가 2줄 이상 넘어가면 말줄임 표시
    - 메시지 전달 시간 표시 형식 -> '오후 12:36'와 같은 형식으로 수정

--- carrotproject/carrotapp/views.py
+++ carrotproject/carrotapp/views.py
@@ -207,15 +207,20 @@ def trade_post(request, pk):
     post.save()
     comments = Comment.objects.filter(post=post).order_by("-created_date")
 
+    # 가장 최근 생성된 채팅방
+    recent_chatroom = ChatRoom.objects.filter(product_id=post.id).order_by("-created_at").first()
+
     context = {
         "post": post,
         "comments": comments,
         "form": CommentForm(),
+        "recent_chatroom": recent_chatroom
     }
 
     return render(request, "dangun_app/trade_post.html", context)
 
 # 채팅방 생성
+@login_required
 def create_chatroom(request, post_id):
     if request.method == "POST":
         host = PostProduct.objects.get(id=post_id).author_id
